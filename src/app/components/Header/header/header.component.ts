8cb5984f2d908926759e9619ba339506d94b958a
OppenSource <131401291+OppenSource@users.noreply.github.com>
2024-01-04 08:34:45 +0100

    composant header

--- src/app/components/Header/header/header.component.ts
+++ src/app/components/Header/header/header.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent  implements OnInit {
+export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  ngOnInit(): void {
+    console.log();
+  }
 }
