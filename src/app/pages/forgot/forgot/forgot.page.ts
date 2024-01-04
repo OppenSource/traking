c9c3a6897a48111d9864fae44d403a0697ced3b6
OppenSource <131401291+OppenSource@users.noreply.github.com>
2024-01-04 09:10:15 +0100

    page de réinitialisation de mot de passe

--- src/app/pages/forgot/forgot/forgot.page.ts
+++ src/app/pages/forgot/forgot/forgot.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NavController } from '@ionic/angular';
+import { ToastService } from 'src/app/services/Toast/toast.service';
 
 @Component({
   selector: 'app-forgot',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot.page.scss'],
 })
 export class ForgotPage implements OnInit {
+  forgot: FormGroup | any;
+  account: any = {
+    account: '',
+  };
 
-  constructor() { }
+  constructor(
+    public formBuilder: FormBuilder,
+    private toast: ToastService,
+    public navCtrl: NavController
+  ) {}
 
   ngOnInit() {
+    this.forgot = this.formBuilder.group({
+      account: ['', [Validators.required]],
+    });
   }
 
+  get errorControl() {
+    return this.forgot.controls;
+  }
+
+  submitForm = () => {
+    if (this.forgot.valid) {
+      this.account.account = this.forgot.value.account;
+      setTimeout(() => {
+        this.toast.successLogin();
+        this.navCtrl.navigateForward('login');
+      }, 1000);
+
+      return false;
+    } else {
+      this.toast.presentToast();
+      return console.log('');
+    }
+  };
 }
