ed5e7170ddf6d9fc212f2ee713df533dec4bf437
OppenSource <131401291+OppenSource@users.noreply.github.com>
2024-01-04 09:07:00 +0100

    page d'enregistrement

--- src/app/pages/Register/register/register.page.ts
+++ src/app/pages/Register/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NavController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,128 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  register: FormGroup | any;
+  isTrue: any = false;
+  account: any = {
+    email: '',
+    phone: '',
+    fullname: '',
+    username: '',
+    password: '',
+    account: '',
+  };
 
-  constructor() { }
+  constructor(
+    public formBuilder: FormBuilder,
+    private toastController: ToastController,
+    public navCtrl: NavController
+  ) {}
 
   ngOnInit() {
+    this.register = this.formBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@institutsaintjean+.org$'),
+        ],
+      ],
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(20),
+        ],
+      ],
+      phone: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(9),
+          Validators.maxLength(6),
+          Validators.pattern(/^6/),
+        ],
+      ],
+      fullname: ['', [Validators.required, Validators.minLength(5)]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(20),
+        ],
+      ],
+      confirmPassword: ['', [Validators.required, Validators.minLength(5)]],
+      account: ['', [Validators.required]],
+    });
+    if (this.register.value.account === 'student') {
+      this.isTrue = true;
+    }
   }
 
+  get errorControl() {
+    return this.register.controls;
+  }
+
+  passwordMatchValidator(password: any, confirmPassword: any) {
+    if (password === confirmPassword) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Renseignez tous les champs avant de continuer.',
+      duration: 3000,
+      color: 'danger',
+      icon: 'alert-circle-outline',
+      position: 'bottom',
+    });
+    await toast.present();
+  }
+
+  async passwordToast() {
+    const toast = await this.toastController.create({
+      message: 'Les mots de passe ne sont pas identiques.',
+      duration: 5000,
+      color: 'warning',
+      icon: 'warning-outline',
+      position: 'bottom',
+    });
+    await toast.present();
+  }
+
+  async successRegistration() {
+    const toast = await this.toastController.create({
+      message: 'Le compte a été créer avec succès.',
+      duration: 5000,
+      color: 'success',
+      icon: 'happy-outline',
+      position: 'top',
+    });
+    await toast.present();
+  }
+
+  submitForm = () => {
+    if (this.register.valid) {
+      this.account.phone = this.register.value.phone;
+      this.account.fullname = this.register.value.fullname;
+      this.account.username = this.register.value.username;
+      this.account.password = this.register.value.password;
+      this.account.account = this.register.value.account;
+      setTimeout(() => {
+        this.successRegistration();
+        this.navCtrl.navigateForward('login');
+      }, 1000);
+
+      console.log(this.account);
+      return false;
+    } else {
+      this.presentToast();
+      return '';
+    }
+  };
 }
